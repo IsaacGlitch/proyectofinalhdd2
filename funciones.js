be59d1435a35621845ad3104a95cd231dc8f2a77
IsaacGlitch <vhgcalebalvarez17@gmail.com>
2026-04-09 01:40:08 -0500

    Cambios del apartado de Index, Estilos y Js, agregado de login y resitro y cambio en la seccion de contenido del index

--- funciones.js
+++ funciones.js
@@ -1,3 +1,48 @@
 function agregarCarrito(producto) {
     alert(producto + " agregado al carrito 🛒");
 }
+
+function mostrarLogin() {
+    document.getElementById("loginForm").style.display = "flex";
+}
+
+function mostrarRegistro() {
+    document.getElementById("registroForm").style.display = "flex";
+}
+
+function cerrarModal() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registroForm").style.display = "none";
+}
+
+function registrar() {
+    let user = document.getElementById("regUser").value;
+    let pass = document.getElementById("regPass").value;
+
+    localStorage.setItem(user, pass);
+    alert("Usuario registrado correctamente");
+    cerrarModal();
+}
+
+function iniciarSesion() {
+    let user = document.getElementById("loginUser").value;
+    let pass = document.getElementById("loginPass").value;
+
+    let storedPass = localStorage.getItem(user);
+
+    if (storedPass === pass) {
+        alert("Bienvenido " + user);
+    } else {
+        alert("Datos incorrectos");
+    }
+}
+
+function seleccionarCategoria(event, categoria) {
+    alert("Seleccionaste: " + categoria);
+
+    document.querySelectorAll(".card").forEach(card => {
+        card.classList.remove("seleccionado");
+    });
+
+    event.currentTarget.classList.add("seleccionado");
+}
